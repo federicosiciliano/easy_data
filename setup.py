643cc5196f4885ceb5e372ceff16689e87da5967
siciliano-diag <siciliano@diag.uniroma1.it>
2024-03-17 18:21:02 +0100

    version 1.0.0

--- setup.py
+++ setup.py
@@ -1,32 +1,29 @@
 import os
-
 from setuptools import setup
 
+# Read the contents of the README.md file for the long description
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Initialize an empty list for the installation requirements
 install_requires = []
+
+# Check if a requirements.txt file exists and if so, read its contents
 if os.path.isfile("requirements.txt"):
     with open("requirements.txt") as f:
         install_requires = f.read().splitlines()
 
+# Define the package setup configuration
 setup(
-    name='data_utils',
-    packages=['data_utils'],
-    description='data utils',
-    long_description=long_description,
+    name='Data Utils',  # Replace with your package name
+    packages=['data_utils'],  # List of all packages included in your project
+    description='Data Utils: Simplify Data Utilization',  
+    long_description=long_description,  # Use the contents of README.md as the long description
     long_description_content_type="text/markdown",
-    version='0.0.1',
-    install_requires=install_requires,
-    url='https://github.com/siciliano-diag/data_utils.git',
-    author='siciliano-diag',
+    version='1.0.0',  # Specify the version of your package
+    install_requires=install_requires,  # List of required dependencies
+    url='https://github.com/siciliano-diag/data_utils.git',  # Replace with the URL of your GitHub repository
+    author='Federico Siciliano',
     author_email='siciliano@diag.uniroma1.it',
-    keywords=['pip','MachineLearning']
-    )
-
-
-
-
-#license='MIT',
-#project_urls = {"Bug Tracker": "https://github.com/mike-huls/toolbox/issues"},
-
+    keywords=['MachineLearning', 'Data']  # Keywords related to your package
+)
